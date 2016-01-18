78a3ff61a5de9bbf314792c31ef74e0c0515efc6
Xavier Tô <xavier.to@shopify.com>
2016-01-18 17:15:22 -0500

    initial commit

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './request_logger'
+
+run RequestLogger
---
+++ mash_temperature_monitor.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'json'
+require "serialport"
+
+#params for serial port
+
+
+class RequestLogger < Sinatra::Base
+  get /temperatures/ do
+    headers 'Access-Control-Allow-Origin' => '*'
+    content_type :json
+
+    {temperatures: [
+      {
+        "temperature" => read_from_serial.to_f,
+        "readAt" => Time.now.to_s,
+        "unit" => "C"
+      }
+    ]}.to_json
+  end
+
+  def read_from_serial
+    port_str = "/dev/cu.usbmodem1411"
+    baud_rate = 9600
+    data_bits = 8
+    stop_bits = 1
+    parity = SerialPort::NONE
+
+    serial_port_reading = ""
+    serial_port = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+    serial_port.read_timeout=300
+    serial_port. << "X"
+    sleep(0.5)
+    serial_port_reading = serial_port.readline("\r\n")
+    serial_port.close
+    return serial_port_reading
+  end
+end
